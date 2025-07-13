b5148d0387d9dea5c450c8ed52c15cd786396267
RinY0shida <tororo1219@gmail.com>
2025-07-13 18:56:34 +0900

    [code]fix test.py

--- test.py
+++ test.py
@@ -10,19 +10,18 @@ xml_path = "models/inverted_pendulum.xml"
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 data.qpos[0] = np.deg2rad(20)
-Kp = 1003.0
+Kp = 13.0
 Kd =5.0
 #ビューアを起動
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
-       # theta = data.qpos[0]
-       # omega = data.qvel[0]
-       # torque = -Kp * theta - Kd * omega
+        # theta = data.qpos[0]
+        # omega = data.qvel[0]
+        # torque = -Kp * theta - Kd * omega
  
-       # disturbance = np.random.normal(loc=0.0, scale=1.0)
-        #data.ctrl[0] = torque + disturbance
-        data.ctrl[0] = np.random.uniform(-180.0, 180.0)
- 
+        # disturbance = np.random.normal(loc=0.0, scale=1.0)
+        # data.ctrl[0] = torque + disturbance
+        data.ctrl[0] = np.random.uniform(-90.0, 90.0)
         mujoco.mj_step(model, data)
  
         viewer.sync()                # ステップ後の状態をviewerに反映
