88431207452685b203762811b6a4278d8a6d35f1
RinY0shida <tororo1219@gmail.com>
2025-07-18 01:45:55 +0900

    update test.py

--- test.py
+++ test.py
@@ -11,22 +11,6 @@ xml_path = "models/inverted_pendulum.xml"
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 data.qpos[0] = np.deg2rad(40)
-# Kp = 2
-# Kd = 2
-
-# with mujoco.viewer.launch_passive(model, data) as viewer:
-#     while viewer.is_running():
-#         theta = data.qpos[0]
-#         omega = data.qvel[0]
-#         torque = -Kp * theta - Kd * omega
-#         print("deg:", theta)
-#         print("torque:", torque)
-#         torque = np.clip(torque, -2, 2)
-#         data.ctrl[0] = torque
-#         mujoco.mj_step(model, data)
-        
-#         viewer.sync()
-#         time.sleep(model.opt.timestep)
  
 # ゲイン　発散しまくりでおわおわり
 # Kp = 35.0
@@ -37,7 +21,7 @@ Kd = 1.0
 
 target_angles_deg = [-40, 0, 40]
 target_angles = [deg * 3.1416 / 180.0 for deg in target_angles_deg]
-interval = 5.0
+interval = 2.0
 start_time = time.time()
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
@@ -52,6 +36,8 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
 
         error = theta - target_theta
         torque = -Kp * error - Kd * omega
+        print("error:",error);
+        print("omega:",omega);
         data.ctrl[0] = torque
 
         mujoco.mj_step(model, data)
