2c11903d3ccae98ba873f0f0e8096823eac7f271
RinY0shida <tororo1219@gmail.com>
2026-04-14 17:41:04 +0900

    update sin wave

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import mujoco
 import mujoco.viewer
 import numpy as np
 import time
+import math
 
 xml_path = "models/inverted_pendulum.xml"
 
@@ -12,7 +13,6 @@ model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 data.qpos[0] = np.deg2rad(40)
  
-# ゲイン　発散しまくりでおわおわり
 # Kp = 35.0
 # Kd = 5.0
 
@@ -24,7 +24,12 @@ target_angles = [deg * 3.1416 / 180.0 for deg in target_angles_deg]
 interval = 2.0
 start_time = time.time()
 
+# motor_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, "motor1")
+# muscle1_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, "muscle1")
+# muscle2_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, "muscle2")
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
+    count = 0
     while viewer.is_running():
         elapsed = time.time() - start_time
 
@@ -38,8 +43,19 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         torque = -Kp * error - Kd * omega
         print("error:",error);
         print("omega:",omega);
-        data.ctrl[0] = torque
+        
+        
+        if count > 1:
+            count = 0
+        else:
+            count = count + 0.01
 
+        sin_wave_1 = math.sin(count)
+        sin_wave_2 = -math.sin(count)
+        
+        data.ctrl[1] = sin_wave_1 
+        data.ctrl[2] = sin_wave_2
+        
         mujoco.mj_step(model, data)
         viewer.sync()
         time.sleep(model.opt.timestep)
